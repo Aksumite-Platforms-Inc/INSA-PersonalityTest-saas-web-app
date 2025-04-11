c598b274421c06cbc5ed4cba7bbee91cc73e47cb
Yordanos Yeshitla <yordiman614@gmail.com>
2025-04-11 09:58:18 +0300

    Refactor user roles and update routing for admin dashboards; remove superadmin role and layouts

---
+++ src/layouts/BranchAdminLayout.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+const BranchAdminLayout = () => <div>Branch Admin Dashboard</div>;
+
+export default BranchAdminLayout;
---
+++ src/layouts/EmployeeLayout.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+const EmployeeLayout = () => <div>Employee Dashboard</div>;
+
+export default EmployeeLayout;
