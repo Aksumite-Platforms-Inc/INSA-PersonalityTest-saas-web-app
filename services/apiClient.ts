11296c4498e1c5acee790fee49c3edd9663e1982
Henok Samson <henoksamson57@gmail.com>
2025-11-17 15:23:30 +0300

    Use environment API base URL

--- services/apiClient.ts
+++ services/apiClient.ts
@@ -6,8 +6,8 @@ import toast from "react-hot-toast";
 
 const apiClient = axios.create({
 // baseURL:
-//     process.env.NEXT_PUBLIC_API_URL || "https://personality.insa.gov.et/api/v1",  
-  baseURL: "http://localhost:8080/api/v1",
+    process.env.NEXT_PUBLIC_API_URL || "https://personality.insa.gov.et/api/v1",  
+  // baseURL: "http://localhost:8080/api/v1",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
